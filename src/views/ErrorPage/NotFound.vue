f7060e17b90401c739bc9228b33a0544ed0be8d9
MasihTak <masihtak@outlook.com>
2021-02-14 17:12:01 +0100

    [WIP] modify 404 view

--- src/views/ErrorPage/NotFound.vue
+++ src/views/ErrorPage/NotFound.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="container">
-    404 | page not found!
+    <div class="row">
+      <div class="error">
+        <img src="../../assets/404.svg" alt="404 Error in morse code">
+        <p>Page Not Found!</p>
+        <p>Sorry, we are unable to find the page you are looking for. Click the blow button to return home.</p>
+        <router-link class="button button-primary" to="/">Home</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "NotFound.vue"
+  name: "NotFound.vue"
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "ErrorPage.scss";
 </style>
