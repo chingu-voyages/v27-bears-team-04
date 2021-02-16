434fdf202c3f6d734c8bbd427f1d5b7f828270b5
Sara <sbr.90@hotmail.com>
2021-02-16 21:27:47 +0100

    user interfaces of homepage, terms, policies, login pages

--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -1,18 +1,57 @@
+
 <template>
-  <div class="homepage">
-    <h1>This is HomePage</h1>
-    <router-link to="/about">About</router-link><br>
-    <router-link to="/privacy">Privacy</router-link><br>
-     <router-link to="/terms">Terms</router-link><br>
-      <router-link to="/register">Register</router-link><br>
-       <router-link to="/login">Login</router-link><br>
+
+<div class="c-main">
+  <div class="c-homepage">
+    
+    <h1 class="c-homepage__title">IMorse</h1>
+    <h1 class="c-homepage__subtitle">
+      Secretive Communication Social Platform Based On Morse Code! Make Friends
+      While Talking Morse.
+    </h1>
+    <div>
+    </div>
+    <div class="c-buttons-container">
+      <router-link to="/login" class="c-buttons-container__login"
+        >Log In</router-link
+      >
+      <router-link to="/register" class="c-buttons-container__signin"
+        >Sign In</router-link
+      >
+    </div>
+
   </div>
-</template>
+   <div class="c-menu">
+      <router-link to="/about" class="c-menu__item">About</router-link>
+            <router-link to="/about" class="c-menu__item">Contact</router-link>
+
+      <div class="c-menu__social"></div>
+    </div>
+     <div class="c-homepage__img-container">
+        <img class="c-homepage__img-container__img" v-bind:src="img" alt="" />
+      </div>
+</div>
+  <!-- 
+ <div class="c-footer">
+    <router-link to="/privacy">Privacy</router-link>
+    <router-link to="/terms">Terms</router-link>
+  </div> -->
+</template> 
 
 <script>
+const morseCode = require('../../assets/images/morse-machine.jpg');
+
+
 export default {
+
   name: "Home",
+    data(){
+        return{
+            img: morseCode,
+        }
+    }
 };
+
 </script>
 
 
