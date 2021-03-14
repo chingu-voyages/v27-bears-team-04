250000bff944487ae182d6a8a6bfdf87960a0531
MasihTak <masihtak@outlook.com>
2021-03-14 13:50:34 +0100

    Applied style to the login page

--- src/views/LoginPage/LoginPage.vue
+++ src/views/LoginPage/LoginPage.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="login-container">
-    <h1 class="login-title">LOGIN</h1>
-    <div class="c-login-buttons">
-    <input class="c-login-buttons__item" placeholder="Enter Your Email"/>
-    <input class="c-login-buttons__item" placeholder="Enter Your Password"/>
-    <button class="c-login-buttons__submit" type="submit"> Submit </button>
+  <div class="container">
+    <h1 class="my-5">Login</h1>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <form>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input class="form-control"
+                       type="email"
+                       id="email"
+                       v-model="email"
+                />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input class="form-control"
+                       type="password"
+                       id="password"
+                       v-model="password"
+                />
+              </div>
+            </div>
+          </div>
+        </form>
+        <button class="btn btn--primary d-grid mx-auto d-md-inline mt-5" type="submit">Submit</button>
+      </div>
     </div>
-
-
   </div>
 </template>
 
